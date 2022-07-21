8a8715d9958bd707b9535190d363bde645d695bc
Ben White <bwhite@swinetechnologies.com>
2022-07-21 16:27:24 -0500

    updating versions

--- lib/src/chat_view.dart
+++ lib/src/chat_view.dart
@@ -404,7 +404,7 @@ class DashChatState extends State<DashChat> {
     scrollController = widget.scrollController ?? ScrollController();
     textController = widget.textController ?? TextEditingController();
     inputFocusNode = widget.focusNode ?? FocusNode();
-    WidgetsBinding.instance!.addPostFrameCallback(widgetBuilt);
+    WidgetsBinding.instance.addPostFrameCallback(widgetBuilt);
     super.initState();
   }
 
